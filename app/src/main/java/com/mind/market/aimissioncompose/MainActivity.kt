78129f0afd17648d207b6ce23bddf8a4a4128e77
IlMike22 <michaelwidlok@yahoo.de>
2022-11-26 09:38:06 +0100

    add top app bar compose

--- app/src/main/java/com/mind/market/aimissioncompose/MainActivity.kt
+++ app/src/main/java/com/mind/market/aimissioncompose/MainActivity.kt
@@ -4,19 +4,15 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.*
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -27,95 +23,75 @@ import com.mind.market.aimissioncompose.presentation.detail.DetailScreen
 import com.mind.market.aimissioncompose.presentation.landing_page.LandingPageScreen
 import com.mind.market.aimissioncompose.ui.theme.AimissionComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AimissionComposeTheme {
                 val navController = rememberNavController()
                 val scaffoldState: ScaffoldState = rememberScaffoldState()
-                val sheetState: BottomSheetState =
-                    rememberBottomSheetState(
-                        initialValue = BottomSheetValue.Collapsed,
-                        animationSpec = spring(
-                            dampingRatio = Spring.DampingRatioMediumBouncy
-                        )
-                    )
-                val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
-                    bottomSheetState = sheetState
-                )
-                val scope = rememberCoroutineScope()
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    scaffoldState = scaffoldState
-                ) {
-                    BottomSheetScaffold(
-                        scaffoldState = bottomSheetScaffoldState,
-                        sheetContent = {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(250.dp),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Text(
-                                    text = "Bottom Sheet Content",
-                                    fontSize = 16.sp,
-                                )
-                            }
-                        },
-                        sheetBackgroundColor = Color.Blue,
-                        sheetPeekHeight = 36.dp
-                    ) {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "AimissionCompose")
+                            },
+                            actions = {
+                                // RowScope here, so these icons will be placed horizontally
+                                IconButton(onClick = { /* doSomething() */ }) {
+                                    Icon(
+                                        Icons.Filled.Create,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                                IconButton(onClick = { /* doSomething() */ }) {
+                                    Icon(
+                                        Icons.Filled.Settings,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            },
+                            backgroundColor = MaterialTheme.colors.primary,
+                            contentColor = Color.White,
+                            elevation = 10.dp
+                        )
+                    }, content = {
+                        NavHost(
+                            navController = navController,
+                            startDestination = Route.LandingPage
                         ) {
-                            Button(
-                                onClick = {
-                                    scope.launch {
-                                        if (sheetState.isCollapsed) sheetState.expand() else sheetState.collapse()
-                                    }
-                                }) {
-                                Text("Show/Hide Bottom Sheet")
+                            composable(Route.LandingPage) {
+                                Surface(
+                                    modifier = Modifier.fillMaxSize(),
+                                    color = MaterialTheme.colors.background
+                                ) {
+                                    LandingPageScreen(
+                                        navController = navController
+                                    )
+                                }
                             }
-
-                        }
-                    }
-                    NavHost(
-                        navController = navController,
-                        startDestination = Route.LandingPage
-                    ) {
-                        composable(Route.LandingPage) {
-                            Surface(
-                                modifier = Modifier.fillMaxSize(),
-                                color = MaterialTheme.colors.background
+                            composable(
+                                route = Route.ADD + "?goalId={goalId}",
+                                arguments = listOf(navArgument(
+                                    name = "goalId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                })
                             ) {
-                                LandingPageScreen(
+                                DetailScreen(
                                     navController = navController
                                 )
                             }
                         }
-                        composable(
-                            route = Route.ADD + "?goalId={goalId}",
-                            arguments = listOf(navArgument(
-                                name = "goalId"
-                            ) {
-                                type = NavType.IntType
-                                defaultValue = -1
-                            })
-                        ) {
-                            DetailScreen(
-                                navController = navController
-                            )
-                        }
                     }
-                }
+                )
             }
         }
     }
--- app/src/main/java/com/mind/market/aimissioncompose/presentation/landing_page/LandingPageScreen.kt
+++ app/src/main/java/com/mind/market/aimissioncompose/presentation/landing_page/LandingPageScreen.kt
@@ -1,32 +1,35 @@
 package com.mind.market.aimissioncompose.presentation.landing_page
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.mind.market.aimissioncompose.navigation.Route
 import com.mind.market.aimissioncompose.presentation.landing_page.components.Goal
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun LandingPageScreen(
     viewModel: LandingPageViewModel = hiltViewModel(),
     navController: NavController
 ) {
     val state = viewModel.state
+    val scope = rememberCoroutineScope()
 
     val detailPageScreenResult = navController.currentBackStackEntry
         ?.savedStateHandle
@@ -34,6 +37,51 @@ fun LandingPageScreen(
     detailPageScreenResult?.value?.let { isUpdateList ->
         if (isUpdateList) {
             viewModel.getGoals()
+        }
+    }
+
+    val sheetState: BottomSheetState =
+        rememberBottomSheetState(
+            initialValue = BottomSheetValue.Collapsed,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy
+            )
+        )
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = sheetState
+    )
+
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetContent = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Bottom Sheet Content",
+                    fontSize = 16.sp,
+                )
+            }
+        },
+        sheetBackgroundColor = Color.Blue,
+        sheetPeekHeight = 36.dp,
+
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = {
+                    scope.launch {
+                        if (sheetState.isCollapsed) sheetState.expand() else sheetState.collapse()
+                    }
+                }) {
+                Text("Show/Hide Bottom Sheet")
+            }
         }
     }
 
