41c81840e9b694f514bcf1c8813d14a5685b6501
IlMike22 <michaelwidlok@gmail.com>
2023-03-22 15:31:17 +0100

    update version info

--- app/src/main/java/com/mind/market/aimissioncompose/data/Constants.kt
+++ app/src/main/java/com/mind/market/aimissioncompose/data/Constants.kt
@@ -2,7 +2,7 @@ package com.mind.market.aimissioncompose.data
 
 const val AUTHOR_NAME = "Author: Michael Widlok"
 const val APP_NAME = "AimissionCompose"
-const val VERSION = "Version: 0.65"
+const val VERSION = "Version: 0.68"
 const val USED_TECHNOLOGIES =
     "Used technologies: Compose, Navigation, ViewModel, Clean Code, Room, Flow, Coroutines" +
-            "Firebase Authentication, Firebase Realtime Database"
+            "Firebase Authentication, Firebase Realtime Database, Dependency Injection with Hilt"
