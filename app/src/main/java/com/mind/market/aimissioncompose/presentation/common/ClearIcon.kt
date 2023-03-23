cd6d71105c2ec26f0b8a540385ff7a5455a24386
IlMike22 <michaelwidlok@gmail.com>
2023-03-23 09:55:17 +0100

    add empty screen when user is not logged in on settings and add goal screen, add clear icon in textfield

---
+++ app/src/main/java/com/mind/market/aimissioncompose/presentation/common/ClearIcon.kt
@@ -0,0 +1,8 @@
+package com.mind.market.aimissioncompose.presentation.common
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ClearIcon() {
+
+}
---
+++ app/src/main/java/com/mind/market/aimissioncompose/presentation/common/UserNotAuthenticatedScreen.kt
@@ -0,0 +1,2 @@
+package com.mind.market.aimissioncompose.presentation.common
+
